16ec7886f1348b12bd9bb2ed8780286c3d43182c
Eric Medvet <eric.medvet@gmail.com>
2023-02-25 22:44:50 +0100

    module-info.java and Main added.

---
+++ io.github.ericmedvet.jsdynsym.core/src/main/java/io/github/ericmedvet/jsdynsym/core/Main.java
@@ -0,0 +1,10 @@
+package io.github.ericmedvet.jsdynsym.core;
+
+/**
+ * @author "Eric Medvet" on 2023/02/25 for jsdynsym
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ io.github.ericmedvet.jsdynsym.core/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author "Eric Medvet" on 2023/02/25 for jsdynsym
+ */module jsdynsym.core {
+  exports io.github.ericmedvet.jsdynsym.core;
+}
