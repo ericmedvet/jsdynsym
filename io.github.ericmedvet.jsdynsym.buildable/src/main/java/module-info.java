1038b7d3d83b3691d5ebc51527c9f3f4d10ca11b
Eric Medvet <eric.medvet@gmail.com>
2023-04-13 17:48:52 +0200

    module-info.java updated.

--- io.github.ericmedvet.jsdynsym.buildable/src/main/java/module-info.java
+++ io.github.ericmedvet.jsdynsym.buildable/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module io.github.ericmedvet.jsdynsym.buildable {
   requires io.github.ericmedvet.jsdynsym.core;
   requires io.github.ericmedvet.jnb.core;
   exports io.github.ericmedvet.jsdynsym.buildable;
+  exports io.github.ericmedvet.jsdynsym.buildable.builders;
   opens io.github.ericmedvet.jsdynsym.buildable.builders to io.github.ericmedvet.jnb.core;
 }
