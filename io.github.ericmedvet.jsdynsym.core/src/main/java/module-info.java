a709874068d921f387285e23a4e8a5b384d1dd3e
Eric Medvet <eric.medvet@gmail.com>
2023-06-16 14:11:04 +0200

    module-info.java fixed.

--- io.github.ericmedvet.jsdynsym.core/src/main/java/module-info.java
+++ io.github.ericmedvet.jsdynsym.core/src/main/java/module-info.java
@@ -2,6 +2,7 @@
  * @author "Eric Medvet" on 2023/02/25 for jsdynsym
  */
 module io.github.ericmedvet.jsdynsym.core {
+  exports io.github.ericmedvet.jsdynsym.grid;
   exports io.github.ericmedvet.jsdynsym.core;
   exports io.github.ericmedvet.jsdynsym.core.composed;
   exports io.github.ericmedvet.jsdynsym.core.numerical;
