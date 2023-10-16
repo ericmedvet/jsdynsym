b6e4d383bac60bf34d60d6f0ad445eff28c0271c
Eric Medvet <eric.medvet@gmail.com>
2023-10-16 17:55:56 +0200

    Licence cleaning after removing Intellij actions on commit.

--- io.github.ericmedvet.jsdynsym.buildable/src/main/java/io/github/ericmedvet/jsdynsym/buildable/builders/Misc.java
+++ io.github.ericmedvet.jsdynsym.buildable/src/main/java/io/github/ericmedvet/jsdynsym/buildable/builders/Misc.java
@@ -1,17 +1,21 @@
-/*
- * Copyright 2023 eric
- *
+/*-
+ * ========================LICENSE_START=================================
+ * jsdynsym-buildable
+ * %%
+ * Copyright (C) 2023 Eric Medvet
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * =========================LICENSE_END==================================
  */
 
 package io.github.ericmedvet.jsdynsym.buildable.builders;
@@ -19,7 +23,6 @@ package io.github.ericmedvet.jsdynsym.buildable.builders;
 import io.github.ericmedvet.jnb.core.Param;
 import io.github.ericmedvet.jsdynsym.core.DoubleRange;
 import io.github.ericmedvet.jsdynsym.grid.Grid;
-
 import java.util.List;
 import java.util.Random;
 import java.util.random.RandomGenerator;
