efebafeed456a4c01aaed5c52d3aa2c648f11168
Eric Medvet <eric.medvet@gmail.com>
2024-02-27 22:05:25 +0100

    Added: interfaces for control task.

---
+++ io.github.ericmedvet.jsdynsym.control/src/main/java/io/github/ericmedvet/jsdynsym/control/SingleAgentTask.java
@@ -0,0 +1,55 @@
+/*-
+ * ========================LICENSE_START=================================
+ * jsdynsym-control
+ * %%
+ * Copyright (C) 2023 - 2024 Eric Medvet
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+package io.github.ericmedvet.jsdynsym.control;
+
+import io.github.ericmedvet.jnb.datastructure.DoubleRange;
+import io.github.ericmedvet.jsdynsym.core.DynamicalSystem;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public interface SingleAgentTask<C extends DynamicalSystem<O, A, ?>, O, A, S> {
+
+  record Step<O, A, S>(O observation, A action, S state) {}
+
+  SortedMap<Double, Step<O, A, S>> simulate(C agent);
+
+  static <C extends DynamicalSystem<O, A, ?>, O, A, S> SingleAgentTask<C, O, A, S> fromEnvironment(
+      DynamicalSystem<A, O, S> environment, A initialAction, DoubleRange tRange, double dT) {
+    return agent -> {
+      environment.reset();
+      agent.reset();
+      double t = tRange.min();
+      Map<Double, Step<O, A, S>> steps = new HashMap<>();
+      O observation = null;
+      while (t <= tRange.max()) {
+        if (observation == null) {
+          observation = environment.step(t, initialAction);
+        }
+        A action = agent.step(t, observation);
+        observation = environment.step(t, action);
+        steps.put(t, new Step<>(observation, action, environment.getState()));
+        t = t + dT;
+      }
+      return new TreeMap<>(steps);
+    };
+  }
+}
