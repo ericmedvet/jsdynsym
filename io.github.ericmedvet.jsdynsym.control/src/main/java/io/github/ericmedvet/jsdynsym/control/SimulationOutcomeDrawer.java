005e43d128026c9677faf9dbfc19fbb937b878cb
Eric Medvet <eric.medvet@gmail.com>
2024-07-15 16:27:12 +0200

    Fixed: clash of interface methods.

--- io.github.ericmedvet.jsdynsym.control/src/main/java/io/github/ericmedvet/jsdynsym/control/SimulationOutcomeDrawer.java
+++ io.github.ericmedvet.jsdynsym.control/src/main/java/io/github/ericmedvet/jsdynsym/control/SimulationOutcomeDrawer.java
@@ -19,20 +19,19 @@
  */
 package io.github.ericmedvet.jsdynsym.control;
 
+import io.github.ericmedvet.jsdynsym.control.Simulation.Outcome;
 import io.github.ericmedvet.jviz.core.drawer.Drawer;
 import io.github.ericmedvet.jviz.core.drawer.ImageBuilder;
 import io.github.ericmedvet.jviz.core.drawer.VideoBuilder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public interface SimulationOutcomeDrawer<S>
-    extends ImageBuilder<Simulation.Outcome<S>>, VideoBuilder<Simulation.Outcome<S>> {
+public interface SimulationOutcomeDrawer<S> extends ImageBuilder<Simulation.Outcome<S>> {
   void drawSingle(Graphics2D g, double t, S s);
 
   default void drawAll(Graphics2D g, SortedMap<Double, S> ss) {}
@@ -44,12 +43,13 @@ public interface SimulationOutcomeDrawer<S>
     return lastDrawer.andThen(allDrawer).build(imageInfo, o.snapshots());
   }
 
-  @Override
-  default Video build(VideoInfo videoInfo, Simulation.Outcome<S> o) throws IOException {
-    Drawer<Map.Entry<Double, S>> drawer = (g, e) -> drawSingle(g, e.getKey(), e.getValue());
-    Function<Simulation.Outcome<S>, SortedMap<Double, Map.Entry<Double, S>>> splitter =
-        lO -> lO.snapshots().entrySet().stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, e -> e, (e1, e2) -> e1, TreeMap::new));
-    return VideoBuilder.from(drawer, splitter).build(videoInfo, o);
+  default VideoBuilder<Simulation.Outcome<S>> videoBuilder() {
+    return (videoInfo, o) -> {
+      Drawer<Map.Entry<Double, S>> drawer = (g, e) -> drawSingle(g, e.getKey(), e.getValue());
+      Function<Outcome<S>, SortedMap<Double, Map.Entry<Double, S>>> splitter =
+          lO -> lO.snapshots().entrySet().stream()
+              .collect(Collectors.toMap(Map.Entry::getKey, e -> e, (e1, e2) -> e1, TreeMap::new));
+      return VideoBuilder.from(drawer, splitter).build(videoInfo, o);
+    };
   }
 }
